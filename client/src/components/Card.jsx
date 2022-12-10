69cdaa51aa08d97aff94d4dcafe18272d2501758
unknown <diego.elprado22@gmail.com>
2022-12-10 11:50:31 -0300

    se agrega ID a la card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,8 +4,9 @@ import "./Card.css"
 
 export default function Cards({ Name, Image, Type, Id,Str }) {
   return (
-    <div className="card-container">
+    <div key={Id} className="card-container">
     <div className="image-container"><Link to={`/pokemon/${Id}`}> <img src={Image} alt="ImageNotFound"></img></Link></div>
+{  typeof Id === "number"? <p># {Id}</p>:<p># {Id.slice(15)}</p>}
    <h1>{Name}</h1>
     <h2>Type: {Type}</h2>
    <h3>Str: {Str}</h3>
--- client/src/components/PokemonCreate.jsx
+++ client/src/components/PokemonCreate.jsx
@@ -156,111 +156,6 @@ export default function PokemonCreate() {
   }, [dispatch]);
 
   return (
-    // <div className="create">
-    
-   
-    //     <Link to="/pokemons">
-    //       <button>Back</button>
-    //     </Link>
-    //   <form className="container-formulario" onSubmit={(e) => handleSubmit(e)}>
-    
-    //       <label>Name: </label>
-    //       <input
-    //         type="text"
-    //         value={input.name}
-    //         name="name"
-    //         onChange={(e) => handleChange(e)}
-    //       />
-     
-    //     {errors.name && <p>{errors.name}</p>}
-
-    //     <label>Image:</label>
-    //     <input
-    //       type="text"
-    //       value={input.image}
-    //       height="250px"
-    //       name="image"
-    //       onChange={(e) => handleChange(e)}
-    //     />
-
-    //     <label>Life:</label>
-    //     <input
-    //       type="number"
-    //       value={input.life}
-    //       name="life"
-    //       onChange={(e) => handleChange(e)}
-    //     />
-
-    //     {errors.life && <p>{errors.life}</p>}
-
-    //     <label> Strength:</label>
-    //     <input
-    //       type="number"
-    //       value={input.strength}
-    //       name="strength"
-    //       onChange={(e) => handleChange(e)}
-    //     />
-
-    //     {errors.strength && <p>{errors.strength}</p>}
-
-    //     <label> Defense:</label>
-    //     <input
-    //       type="number"
-    //       value={input.defense}
-    //       name="defense"
-    //       onChange={(e) => handleChange(e)}
-    //     />
-
-    //     {errors.defense && <p>{errors.defense}</p>}
-
-    //     <label> Speed:</label>
-    //     <input
-    //       type="number"
-    //       value={input.speed}
-    //       name="speed"
-    //       onChange={(e) => handleChange(e)}
-    //     />
-
-    //     {errors.speed && <p>{errors.speed}</p>}
-
-    //     <label> Height:</label>
-    //     <input
-    //       type="number"
-    //       value={input.height}
-    //       name="height"
-    //       onChange={(e) => handleChange(e)}
-    //     />
-
-    //     {errors.height && <p>{errors.height}</p>}
-
-    //     <label> Weight:</label>
-    //     <input
-    //       type="number"
-    //       value={input.weight}
-    //       name="weight"
-    //       onChange={(e) => handleChange(e)}
-    //     />
-
-    //     {errors.weight && <p>{errors.weight}</p>}
-
-    //     <select onChange={(e) => handleSelect(e)}>
-    //       {allTypes.map((t) => (
-    //         <option value={t.name}> {t.name}</option>
-    //       ))}
-    //     </select>
-
-    //     <button type="submit">Create Pokemon </button>
-    //   </form>
-    //   {input.types.map((e) => (
-    //     <div>
-    //       <p>{e}</p>
-    //       <button onClick={() => handleDelete(e)}>X</button>
-    //       {errors.types && <p>{errors.types}</p>}
-    //     </div>
-    //   ))}
-    // </div>
- 
-
  <div className="principal-on">
   <div>
     <Link to = "/pokemons">
@@ -361,12 +256,12 @@ export default function PokemonCreate() {
 
       <select onChange={e => handleSelect(e)}>
         {allTypes.map(t =>(
-          <option value = {t.name}> {t.name}</option>
+          <option key={t.id} value = {t.name}> {t.name}</option>
         ))}
       </select>
       </div>
       {input.types.map(e => 
-      <div className="opciones">
+      <div key={e.id} className="opciones">
          <div>
                   <ul>
                         {e}<button
